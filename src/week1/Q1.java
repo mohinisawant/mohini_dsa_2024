6e216ac09852e584c22b1ba9c7a8d9ff301fac12
mohinisawant <mohinibusiness98@gmail.com>
2024-07-01 15:45:09 +0530

    first dsa commit

---
+++ src/week1/Q1.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Q1 {
+    public static void main(String args[])
+    {
+        System.out.println("First dsa commit");
+        System.out.println("GitId : mohinisawant");
+    }
+}
